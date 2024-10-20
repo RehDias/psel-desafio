180311ea413b5fd031ffefd2e9be107a02bdd47f
renata dias <renata_dias96@live.com>
2024-10-20 20:08:43 -0300

    build: Configuração do sequelize
    realizado a configuração inicial do sequelize para utilizar o postgres e correção do docker compose

--- backend/src/database/config/database.ts
+++ backend/src/database/config/database.ts
@@ -1,13 +1,14 @@
 import 'dotenv/config';
-import { SequelizeOptions  } from 'sequelize-typescript';
+import { SequelizeOptions } from 'sequelize-typescript';
 
-const config: SequelizeOptions = {
+const config: SequelizeOptions = { 
+  dialect: 'postgres',
   username: process.env.DB_USER || 'postgres',
   password: process.env.DB_PASS || 'postgres',
-  database: process.env.DB_NAME || 'project', // substitua o nome do banco quando for utilizar em seu projeto!
+  database: process.env.DB_NAME || 'project',
   host: process.env.DB_HOST || 'localhost',
   port: Number(process.env.DB_PORT) || 5432,
-  dialect: 'postgres'
+  models: [__dirname + '/models/**/*.model.ts']
 };
 
-export = config;
+module.exports = config;
